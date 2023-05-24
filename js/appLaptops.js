f9ffe2b9700b8049709719441295f440da0cf1dd
Yosmel Sanchez Caleo <43645777+kleo09800@users.noreply.github.com>
2023-05-24 10:23:13 -0400

    Add files via upload

---
+++ js/appLaptops.js
@@ -0,0 +1,97 @@
+
+$(document).ready(function () {
+  
+    // FETCHING DATA FROM JSON FILE
+    $.getJSON("json/lap.json", 
+            function (data) {
+        let p = '';
+
+        // ITERATING THROUGH OBJECTS
+        $.each(data, function (key, value) {
+
+            //CONSTRUCTION OF ROWS HAVING
+            // DATA FROM JSON OBJECT
+            p += '<tr>';
+
+            p += '<td>' + 
+                value.Description + '</td>';
+
+            p += '<td>' + 
+                value.Category + '</td>';
+
+            p += '<td>' + 
+                value.Manufacturer + '</td>';
+
+            p += '<td>' + 
+                value.Shipped + '</td>';
+
+            p += '<td>' + 
+                value.Serial + '</td>';
+
+            p += '<td>' + 
+                value.Location + '</td>';
+
+            p += '<td>' + 
+                value.Tag + '</td>';
+
+            p += '</tr>';
+        });
+          
+        //INSERTING ROWS INTO TABLE 
+        $('#table').append(p);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+//     url: 'json/lapJSON.json',
+//     type: 'POST',
+//     data: {
+//         json: jsonData
+//     },
+//     success: function (response) {
+//         var trHTML = '';
+//         $.each(response, function (i, item) {
+//             trHTML += '<tr><td>' + item.rank + '</td><td>' + item.content + '</td><td>' + item.UID + '</td></tr>';
+//         });
+//         $('#records_table').append(trHTML);
+//     }
+// });
+
+
+
+
+// const jsonData = 
+// function updateTable(tableId, jsonData){
+
+//     var tableHTML = "<tr>";
+//     for (var headers in jsonData[0]) {
+//       tableHTML += "<th>" + headers + "</th>";
+//     }
+//     tableHTML += "</tr>";
+  
+//     for (var eachItem in jsonData) {
+//       tableHTML += "<tr>";
+//       var dataObj = jsonData[eachItem];
+//       for (var eachValue in dataObj){
+//         tableHTML += "<td>" + dataObj[eachValue] + "</td>";
+//       }
+//       tableHTML += "</tr>";
+//     }
+  
+//     document.getElementById(tableId).innerHTML = tableHTML;
+//   }
